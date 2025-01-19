e805c037f5af38831681812c0275324f94708bba
Dohyun Kim <62280486+Dohyun-s@users.noreply.github.com>
2025-01-19 13:53:33 +0000

    add install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 git clone https://github.com/flutter/flutter
-echo "export PATH=/workspaces/FE/flutter/bin:$PATH" >> ~/.bashrc
+current_dir=$(pwd)
+echo "export PATH=$current_dir/flutter/bin:$PATH" >> ~/.bashrc
+. ~/.bashrc
 flutter
 sudo apt update && sudo apt install -y ninja-build libgtk-3-dev
-echo "export ANDROID=/workspaces/FE" >> ~/.bashrc
+echo "export ANDROID=$current_dir" >> ~/.bashrc
 echo "export PATH=\$ANDROID/cmdline-tools:\$PATH" >> ~/.bashrc
 echo "export PATH=\$ANDROID/cmdline-tools/latest/bin:\$PATH" >> ~/.bashrc
-echo "export ANDROID_SDK=/workspaces/FE/" >> ~/.bashrc
+echo "export ANDROID_SDK=$current_dir" >> ~/.bashrc
 echo "export PATH=\$ANDROID_SDK:\$PATH" >> ~/.bashrc
+. ~/.bashrc
 flutter config --android-sdk /workspaces/FE/
 flutter doctor --android-licenses
