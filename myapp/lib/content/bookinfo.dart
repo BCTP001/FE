034479fbacf6b76aa1b46ff4b4138125cf0cff4f
Dohyun Kim <62280486+Dohyun-s@users.noreply.github.com>
2025-02-08 15:04:16 +0000

    fix bookinfo.dart

--- myapp/lib/content/bookinfo.dart
+++ myapp/lib/content/bookinfo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class BookDetailsScreen extends StatefulWidget {
   final dynamic book;
@@ -11,15 +12,22 @@ class BookDetailsScreen extends StatefulWidget {
 
 class _BookDetailsScreenState extends State<BookDetailsScreen> {
   Map<String, bool> _expandedSections = {
-    "도서정보": false,
+    "도서정보": true,
     "목차": false,
     "책소개": false,
   };
-  
+  bool isBookmarked = false;
+
   void _toggleSection(String title) {
     setState(() {
       _expandedSections[title] = !_expandedSections[title]!;
     });
+  }
+
+  String cleanText(String text) {
+    return text.replaceAll(RegExp(r'<br\s*/?>', caseSensitive: false), '\n')
+               .replaceAll(RegExp(r'<[^>]*>'), '')
+               .trim();
   }
 
   @override
@@ -39,42 +47,127 @@ class _BookDetailsScreenState extends State<BookDetailsScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              // 책 표지
               _buildBookCover(widget.book['cover']),
               const SizedBox(height: 16),
-              Text(
-                widget.book['title'] ?? '',
-                style: Theme.of(context).textTheme.headlineMedium,
+              // 제목
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween, // Pushes items to the edges
+                children: [
+                  Text(
+                    widget.book['title'] ?? '',
+                    style: GoogleFonts.jua(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      isBookmarked ? Icons.bookmark_added : Icons.bookmark_add,
+                      size: 40,
+                      color: isBookmarked ? Colors.black : Colors.white,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        isBookmarked = !isBookmarked;
+                      });
+                    },
+                  ),
+                ],
               ),
-              const SizedBox(height: 8),
+              const SizedBox(height: 6),
+              // 저자
               Text(
                 widget.book['author'] ?? '',
-                style: Theme.of(context).textTheme.titleMedium,
+                style: GoogleFonts.jua(
+                  fontSize: 20,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 6),
+              Text(
+                '현재 0명의 유저들이 이 책을 읽고 있습니다!',
+                style: GoogleFonts.jua(
+                  fontSize: 15,
+                  color: Color(0xFF037549),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                padding: const EdgeInsets.all(16),
+                child: buildCategoryTags(widget.book['categoryName']),
               ),
               const SizedBox(height: 16),
-              
               // 도서정보
               _buildInfoBox("도서정보", [
-                Text("발행일: ${widget.book['pubDate'] ?? ''}", style: Theme.of(context).textTheme.titleMedium,),
-                Text("${widget.book['itemPage'] ?? ''}쪽"),
-                Text("ISBN: ${widget.book['isbn'] ?? ''}", style: Theme.of(context).textTheme.titleMedium,),
-              ]),
-              
+                Text(
+                  "발행일: ${widget.book['pubDate'] ?? ''}",
+                  style: GoogleFonts.jua(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  "${widget.book['bookinfo']['itemPage'] ?? ''}쪽",
+                  style: GoogleFonts.jua(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  "ISBN: ${widget.book['isbn'] ?? ''}",
+                  style: GoogleFonts.jua(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                ),
+                ]
+              ),
+              const SizedBox(height: 16),
+              // 책소개
+              _buildInfoBox( "책소개", [
+                if (widget.book['description'] != "")
+                  Text(
+                    widget.book['description'],
+                    style: GoogleFonts.jua(
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                  )
+                else
+                  Text(
+                    '책소개가 없습니다.',
+                    style: GoogleFonts.jua(
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                  )
+                ]
+              ),
               const SizedBox(height: 16),
               // 목차
               _buildInfoBox( "목차", [
                 // Assuming the 'contents' is a list of chapter names
-                if (widget.book['bookinfo']['toc'] != null) 
-                  Text(widget.book['bookinfo']['toc'].toString())
+                if (widget.book['bookinfo']['toc'] != "")
+                  Text(
+                    cleanText(widget.book['bookinfo']['toc']) ,
+                    style: GoogleFonts.jua(
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                  )
                 else
-                  Text("No table of contents available."),
-              ]),
-              
-              const SizedBox(height: 16),
-              
-              // 책소개
-              _buildInfoBox( "책소개", [
-                Text(widget.book['description'] ?? 'No description available.'),
-              ]),
+                  Text(
+                    '책의 목차 정보가 없습니다.',
+                    style: GoogleFonts.jua(
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                  )
+                ]
+              ),
+              const SizedBox(height: 40),
             ],
           ),
         ),
@@ -89,6 +182,7 @@ class _BookDetailsScreenState extends State<BookDetailsScreen> {
     return Card(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
       elevation: 2,
+      color: Color(0xFFF5F5DC),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -100,7 +194,11 @@ class _BookDetailsScreenState extends State<BookDetailsScreen> {
               children: [
                 Text(
                   title,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  style: GoogleFonts.jua(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
                 ),
               ],
             ),
@@ -108,7 +206,22 @@ class _BookDetailsScreenState extends State<BookDetailsScreen> {
 
           // 펼쳐질 내용
           AnimatedCrossFade(
-            firstChild: SizedBox.shrink(), 
+            // firstChild: SizedBox.shrink(),
+            firstChild:  Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: SizedBox(
+                height: 60, // Fixed height instead of a ratio
+                child: Text(
+                  cleanText((contentWidgets.first as Text).data ?? ''),
+                  style: GoogleFonts.jua(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                  maxLines: 3,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ),
             secondChild: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
               child: Column(
@@ -118,7 +231,7 @@ class _BookDetailsScreenState extends State<BookDetailsScreen> {
             ),
             crossFadeState:
                 isExpanded ? CrossFadeState.showSecond : CrossFadeState.showFirst,
-            duration: Duration(milliseconds: 300),
+            duration: Duration(milliseconds: 100),
           ),
 
           // 중앙 아래에 화살표 버튼
@@ -142,25 +255,49 @@ class _BookDetailsScreenState extends State<BookDetailsScreen> {
   Widget _buildBookCover(String? coverUrl) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        final screenWidth = MediaQuery.of(context).size.width;
-        final imageWidth = screenWidth * 0.3; // Reduced to 35% of screen width
-        final imageHeight = imageWidth * 1;
-
         return Center(
           child: coverUrl != null
                 ? Image.network(
                     coverUrl,
-                    width: imageWidth,
-                    height: imageHeight,
+                    width: 250,
+                    height: 355,
                     fit: BoxFit.contain,
                     alignment: Alignment.center,
                     errorBuilder: (context, error, stackTrace) {
-                      return Icon(Icons.book, size: imageWidth);
+                      return Icon(Icons.book, size: 250);
                     },
                   )
-                : Icon(Icons.book, size: imageWidth),
+                : Icon(Icons.book, size: 250),
           );
       },
+    );
+  }
+
+  Widget buildCategoryTags(String categoryString) {
+    // Split the string by '>' and remove any whitespace
+    final categories = categoryString.split('>')
+        .map((e) => e.trim())
+        .where((e) => e.isNotEmpty)
+        .toList();
+
+    return Wrap(
+      spacing: 8, // gap between adjacent chips
+      runSpacing: 8, // gap between lines
+      children: categories.map((category) => Container(
+        decoration: BoxDecoration(
+          color: Color(0xFF28DF99),
+          borderRadius: BorderRadius.circular(16),
+          // border: Border.all(color: Color(0xFF7EEDC3)),
+        ),
+        child: Text(
+          '#'+category,
+          style: GoogleFonts.jua(
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+      )).toList(),
     );
   }
 }
